1e1ee972ceea4799b2a765b75936d18cb9357895
2oothpick <kobbyboateng14@gmail.com>
2024-04-13 02:59:21 +0000

    Add filter_datum function

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Filter logger module
+"""
+import re
+
+
+def filter_datum(
+    fields: list[str], redaction: str, message: str, separator: str
+) -> str:
+    for field in fields:
+        message = re.sub(f"((?<={field}=)[^{separator}]*)", redaction, message)
+    return message
