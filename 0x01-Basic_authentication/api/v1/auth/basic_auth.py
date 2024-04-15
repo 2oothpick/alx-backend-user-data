d8bb776b58634e1e62b2f015dd0bc7acd606eca0
2oothpick <kobbyboateng14@gmail.com>
2024-04-15 23:25:19 +0000

    Add extract_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,10 @@ class BasicAuth(Auth):
     BasicAuth class
     inherits from Auth
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if authorization_header and type(authorization_header) == str:
+            if authorization_header[:6] == 'Basic ':
+                return authorization_header[7:]
+        return None
