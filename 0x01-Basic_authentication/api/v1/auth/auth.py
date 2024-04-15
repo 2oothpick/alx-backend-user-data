5fd5c315b513290655233460b1cca20bb61000ef
2oothpick <kobbyboateng14@gmail.com>
2024-04-15 19:07:32 +0000

    fix style errors

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,22 +6,24 @@ Authentication class
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """
     Authentication class
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         require_auth function
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """
         authorization_header function
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """
         current_user function
