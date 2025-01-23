8ed5c4c896ed8045fd813dac4e8ce84fd183b17c
2oothpick <kobbyboateng14@gmail.com>
2025-01-23 19:59:44 +0000

    Add hash_password function

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""  
+Password encryption module
+"""
+from bcrypt import hashpw, gensalt, checkpw
+
+
+def hash_password(password: str) -> bytes:
+    """  
+    Returns a salted hashed password
+    using bcrypt
+    """
+    salt = gensalt()
+    hash = hashpw(password.encode('utf-8'), salt)
+    return salt
