3b50d15ebeb61791c818d6dae6727af4140e393e
2oothpick <kobbyboateng14@gmail.com>
2025-01-23 20:01:04 +0000

    Fix pycodestyle errors

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""  
+"""
 Password encryption module
 """
 from bcrypt import hashpw, gensalt, checkpw
 
 
 def hash_password(password: str) -> bytes:
-    """  
+    """
     Returns a salted hashed password
     using bcrypt
     """
