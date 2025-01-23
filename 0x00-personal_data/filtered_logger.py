d0f1e965a0623d47e80ffe0e22a2971ec2dcc075
2oothpick <kobbyboateng14@gmail.com>
2025-01-23 20:31:40 +0000

    Add RedactingFormatter class Class customizes Formatter class to obfuscate PII

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Filter logger module
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -15,3 +16,22 @@ def filter_datum(
     for field in fields:
         message = re.sub(f"((?<={field}=)[^{separator}]*)", redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Obfuscates personal data fields from record
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
